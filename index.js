cd76891454b596cfca12606adf522ab44081908a
saigonga <110588183+saigonga@users.noreply.github.com>
2025-02-22 15:51:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+document.getElementById('btn1').addEventListener('click', function() {
+    window.open("https://github.com/", "_blank");
+  });
+  document.getElementById('btn2').addEventListener('click', function() {
+    window.open("https://www.frontendmentor.io/home", "_blank");
+  });
+  document.getElementById('btn3').addEventListener('click', function() {
+    window.open("https://www.linkedin.com/feed/", "_blank");
+  });
+  document.getElementById('btn4').addEventListener('click', function() {
+    window.open("https://x.com/?lang=en&mx=2", "_blank");
+  });
+  document.getElementById('btn5').addEventListener('click', function() {
+    window.open("https://www.instagram.com/", "_blank");
+  });
+  
